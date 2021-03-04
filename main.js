33ee4054307bf873c5bc641343dffb8a9ee4bf7c
thestormshadow <javiervargasruiz94@gmail.com>
2021-03-04 10:46:05 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,7 @@ const mysql = require('mysql')
 const app = express();
 const port = process.env.PORT || 3001;
 
-const connection = mysql.createConnection({
-    host: 'remotemysql.com',
-    user: 'twTR9Q3d2t',
-    password: 'QDQFCdT0nl',
-    database: 'twTR9Q3d2t',
-    port: 3306
- });
+let mysql_query = require('./mysqlconn');
 
 let cars = [];
 
@@ -21,35 +15,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-connection.connect(function(error){
-    if(error){
-       console.log(error);
-    }else{
-       console.log('DB Ready.');
-    }
- });
-
- connection.on('error', err => {
-    console.log(err);
-});
-
-app.get('/', (req, res) => {
-    res.send('Wellcome');
-});
 
 app.get('/cars', (req, res) => {
-    var query = connection.query("select * from cars", [], function(error, result){
+    mysql_query("select * from cars", [], function(error, result){
         if(error)
-           console.log(error);
+            console.log(error); 
         res.json(result);
     });    
 });
 
 app.get('/maintenancePerson/:idc', (req, res) => {
     const idc = req.params.idc;
-    var query = connection.query("select * from maintenance where idcar="+idc, [], function(error, result){
+    mysql_query("select * from maintenance where idcar="+idc, [], function(error, result){
         if(error)
-            console.log(error);
+            console.log(error); 
         res.json(result);
     });    
 });
@@ -57,15 +36,19 @@ app.get('/maintenancePerson/:idc', (req, res) => {
 app.post('/register', (req, res) => {
     const obj = req.body;
     console.log(obj);
-    var query = connection.query("insert into maintenance(idcar,person,date) values(?, ?, ?)", [obj.idCar,obj.person,obj.date], function(error, result){
-        if(error)
-           console.log(error);       
-    });
-
-    var query = connection.query("update cars set inMaintenance = 1 where id = ?", [obj.idCar], function(error, result){
-        if(error)
-           console.log(error);      
-    });
+    if(obj != null){
+        mysql_query("insert into maintenance(idcar,person,date) values(?, ?, ?)", [obj.idCar,obj.person,obj.date], function(error, result){
+            if(error)
+                console.log(error);     
+            
+            mysql_query("update cars set inMaintenance = 1 where id = ?", [obj.idCar], function(error, result){
+                if(error)
+                    console.log(error);    
+            });
+        });   
+        
+    }
+    
 });
 
 app.listen(port, () => console.log("Listening on port " + port));
